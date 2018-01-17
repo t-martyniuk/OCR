f6399cdce8e884c48e870856aa31f5d812ed3e69
t-martyniuk <>
2018-01-17 16:19:24 +0200

    latest working version

--- project1.py
+++ project1.py
@@ -3,18 +3,16 @@ import numpy as np
 import math
 import glob
 
+kernel = np.ones((3,3),np.uint8)
+const_of_overlapping = 0.25
 
 def overlap(a, b, c, d):
     if ((a - c) * (d - b) > 0):
         return True
-    elif ((d - a) * (b - c) > 0) and min(abs(a - d), abs(b - c)) > 0.25 * max(abs(b - a), abs(d - c)):
+    elif ((d - a) * (b - c) > 0) and min(abs(a - d), abs(b - c)) > const_of_overlapping * max(abs(b - a), abs(d - c)):
         return True
     else:
         return False
-
-
-import os
-
 
 def noisy(image):
     row, col, ch = image.shape
@@ -27,28 +25,36 @@ def crop_minAreaRect(img, rect):
     # rotate img
     angle = rect[-1]
     rows,cols = img.shape[0], img.shape[1]
-    M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
-    img_rot = cv2.warpAffine(img,M,(cols,rows))
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),-angle,1)
+    cos = np.abs(M[0,0])
+    sin = np.abs(M[0,1])
+    nW = int(rows * sin + cols * cos)
+    nH = int(rows * cos + cols * sin)
+    M[0,2] += nW/2 - cols/2
+    M[1,2] += nH/2 - rows/2
+
+    img_rot = cv2.warpAffine(img,M,(nW, nH))
 
     # rotate bounding box
-    rect0 = (rect[0], rect[1], 0.0)
     box = cv2.boxPoints(rect)
-    pts = np.int0(cv2.transform(np.array([box]), M))[0]
-    pts[pts < 0] = 0
+    # print(np.array([box]))
+    pts = cv2.transform(box, M)
 
     # crop
-    img_crop = img_rot[pts[1][1]:pts[0][1],
-                       pts[1][0]:pts[2][0]]
+    img_crop = img_rot[pts[3][1]:pts[0][1],
+                       pts[3][0]:pts[2][0]]
 
     return img_crop
 
 def detect_characters(indx):
 
     strip = strips[indx]
+    print(indx)
     cv2.imshow("detected line", strip)
     cv2.waitKey(0)
     # samples = np.empty((0, 100))
 
+    # gray_strip = strip
     gray_strip = cv2.cvtColor(strip, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray_strip, (5, 5), 0)
     thresh = cv2.adaptiveThreshold(blur, 255, 1, 1, 3, 2)
@@ -130,7 +136,7 @@ def detect_characters(indx):
 def detect_lines(src):
     cv2.imshow('start', src)
     cv2.waitKey(0)
-    #Convert to gray
+    # Convert to gray
     src_grey = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
     #blur = cv2.GaussianBlur(src_grey, (5, 5), 0)
 
@@ -172,17 +178,25 @@ def detect_lines(src):
     strips = []
     cv2.imshow("detected lines&contours", im2)
     cv2.waitKey(0)
-    #Detect skewed bounding rectangles
-    len_angles = len(contours)
-    angles = [cv2.minAreaRect(contours[i])[-1] for i in range(len_angles)]
 
-    mean = np.mean(np.array(angles))
-    sd = np.std(np.array(angles))
-    final_contours = [contours[i] for i in range(len_angles) if abs(angles[i] - mean) < 2 * sd]
-    final_angles = [angles[i] for i in range(len_angles) if abs(angles[i] - mean) < 2 * sd ]
-
-    for cnt in final_contours:
-        strips.append(crop_minAreaRect(src,cnt))
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        # cv2.rectangle(src, (x,y),(x+w,y+h),(0,255,0),1)
+        if (h > 2) and (w > 2) and (cv2.contourArea(cnt) > 220):
+            strips.append(src[y + 1:y + h - 1, x + 1:x + w - 1])
+    # #Detect skewed bounding rectangles
+    # len_angles = len(contours)
+    # angles = [cv2.minAreaRect(contours[i])[-1] for i in range(len_angles)]
+    # mean = np.mean(np.array(angles))
+    # sd = np.std(np.array(angles))
+    # final_contours = [contours[i] for i in range(len_angles) if abs(angles[i] - mean) < 2 * sd]
+    # final_angles = [angles[i] for i in range(len_angles) if abs(angles[i] - mean) < 2 * sd ]
+    #
+    # for cnt in final_contours:
+    #     print(cv2.minAreaRect(cnt))
+    #     strip = crop_minAreaRect(src_grey,cv2.minAreaRect(cnt))
+    #     # if (strip.shape[0] > 0) and (strip.shape[1] > 0):
+    #     #     strips.append(strip)
 
         # if cv2.contourArea(cnt) > 220:
         #     x, y, w, h = cv2.boundingRect(cnt)
@@ -192,20 +206,20 @@ def detect_lines(src):
 
     # cv2.imshow('detected contours',src)
     # cv2.waitKey(0)
-
-
-
 #images = glob.glob("image_samples_with_fonts/train/*.bmp")
 #images = glob.glob("*.JPG")
-images = glob.glob("text_skew_inputs.png")
-
-kernel = np.ones((3,3),np.uint8)
+images = glob.glob("pictures/polska.jpg")
 
 #For all images
 for img in images:
     src = cv2.imread(img)
     #Detect the lines in the image
     strips = detect_lines(src)
+    print(len(strips))
+    for strip in strips:
+        cv2.imshow('strip', strip)
+        cv2.waitKey(0)
+
     #For all lines
     for i in range(len(strips)):
         #Detect the squared characters
